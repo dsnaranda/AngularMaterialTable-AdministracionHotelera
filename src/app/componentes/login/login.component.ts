f5ef77cdb5cc1dfa7f420206e031c5b65df02a27
MegaLodias <derikaranda@gmail.com>
2025-01-12 00:49:03 -0500

    Mejoras en el login

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,39 +1,69 @@
 import { Component } from '@angular/core';
 import { CasaService } from '../../servicios/casa.service';
 import { RouterLink } from '@angular/router';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card'; 
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';  
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule ],
+  imports: [CommonModule, FormsModule, MatProgressSpinnerModule, MatCardModule ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
+  animations: [
+    trigger('fadeOut', [
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('1s', style({ opacity: 0 })),
+      ])
+    ])
+  ]
 })
 export class LoginComponent {
   correo: string = '';
   password: string = '';
-  errorMessage: string = '';
+  errorMessage: string | null = null;
+  isLoading: boolean = false;
+  
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, private router: Router,) {}
 
   onSubmit(): void {
+    this.isLoading = true;
     const body = { correo: this.correo, password: this.password };
+  
+    setTimeout(() => {
+      this.http.post<any>('http://localhost:3000/api/login', body).subscribe({
+        next: (response) => {
+          localStorage.setItem('user', JSON.stringify(response.usuario)); // Puedes almacenar el usuario o el token
+          this.router.navigate(['/administracion']);
+          this.isLoading = false;
+        },
+        error: (error) => {
+          this.errorMessage = error.error.error || 'Error desconocido al iniciar sesión';
+          this.isLoading = false;
+          this.checkAndHideError();
+        }
+      });
+    }, 2000); 
+  }
 
-    this.http.post<any>('http://localhost:3000/api/login', body).subscribe({
-      next: (response) => {
-        // Guardar los datos del usuario en localStorage para mantener la sesión activa
-        localStorage.setItem('user', JSON.stringify(response.usuario)); // Puedes almacenar el usuario o el token
+  // Método que borra el mensaje de error
+  hideError() {
+    this.errorMessage = null;
+  }
 
-        // Redirigir a la página de administración
-        this.router.navigate(['/administracion']);
-      },
-      error: (error) => {
-        this.errorMessage = error.error.error || 'Error desconocido al iniciar sesión';
-      }
-    });
+  // Método que revisa si hay un error y lo oculta después de un tiempo
+  checkAndHideError(): void {
+    if (this.errorMessage) {
+      setTimeout(() => {
+        this.hideError();
+      }, 2000); // Borra el mensaje después de 3 segundos
+    }
   }
 }
