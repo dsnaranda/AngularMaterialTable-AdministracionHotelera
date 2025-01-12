50b4cbbfa8445a8b188422c22965d747bc5bf5b6
MegaLodias <derikaranda@gmail.com>
2025-01-12 00:10:27 -0500

    dashboard

--- src/app/componentes/administracion/administracion.component.ts
+++ src/app/componentes/administracion/administracion.component.ts
@@ -2,16 +2,19 @@ import { Component, ViewChild, AfterViewInit, OnInit, ChangeDetectorRef } from '
 import { casa } from '../../entidades/casa';
 import { CasaService } from '../../servicios/casa.service';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card'; 
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { AggEditComponent } from '../agg-edit/agg-edit.component';
 import { HttpClientModule } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-administracion',
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule, MatSortModule, AggEditComponent, HttpClientModule],
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule, AggEditComponent, HttpClientModule, MatCardModule, MatProgressBarModule, CommonModule],
   templateUrl: './administracion.component.html',
   styleUrl: './administracion.component.css'
 })
@@ -25,19 +28,43 @@ export class AdministracionComponent implements AfterViewInit, OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
+  porcentajeWifi: number = 0;
+  porcentajeLavanderia: number = 0;
+  porcentajeSinWifiLavanderia: number = 0;
+  user: any = {}; 
+
+
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort; // Asegura que el sort también se aplique
   }
 
-  constructor(private cservice: CasaService, private changeDetector: ChangeDetectorRef, private router: Router) {}
+  calcularPorcentajes(): void {
+    const totalCasas = this.Lista.length;
+
+    if (totalCasas > 0) {
+      const casasConWifi = this.Lista.filter(casa => casa.wifi).length;
+      const casasConLavanderia = this.Lista.filter(casa => casa.lavanderia).length;
+      const casasSinWifiLavanderia = this.Lista.filter(casa => !casa.wifi && !casa.lavanderia).length;
+
+      this.porcentajeWifi = (casasConWifi / totalCasas) * 100;
+      this.porcentajeLavanderia = (casasConLavanderia / totalCasas) * 100;
+      this.porcentajeSinWifiLavanderia = (casasSinWifiLavanderia / totalCasas) * 100;
+    }
+  }
+  constructor(private cservice: CasaService, private changeDetector: ChangeDetectorRef, private router: Router) { }
 
   ngOnInit(): void {
+    const userData = localStorage.getItem('user');
+    if (userData) {
+      this.user = JSON.parse(userData);  // Parseamos los datos guardados en localStorage
+    }
     // Cargar la lista de casas usando Observable y .subscribe()
     this.cservice.getLista().subscribe(
       (data) => {
         this.Lista = data;
         this.cargarLista(); // Llama a cargarLista para asignar los datos a la tabla
+        this.calcularPorcentajes(); // Llama a calcular los porcentajes y total de casas
         console.log('Casas cargadas:', this.Lista);
       },
       (error) => {
@@ -58,7 +85,7 @@ export class AdministracionComponent implements AfterViewInit, OnInit {
       () => {
         // Eliminamos la casa de la lista localmente
         this.Lista = this.Lista.filter(casa => casa.id !== id);
-        this.cargarLista(); 
+        this.cargarLista();
         this.changeDetector.detectChanges(); // Fuerza la detección de cambios si es necesario
       },
       (error) => {
@@ -66,7 +93,7 @@ export class AdministracionComponent implements AfterViewInit, OnInit {
       }
     );
   }
-  
+
 
   // Método para editar una casa seleccionada
   editCasa(casa: casa): void {
@@ -84,12 +111,8 @@ export class AdministracionComponent implements AfterViewInit, OnInit {
 
   logout() {
     // Eliminar el token o los datos del usuario almacenados en localStorage o sessionStorage
-    localStorage.removeItem('user'); // Si almacenas los datos del usuario en localStorage
-    sessionStorage.removeItem('user'); // Si almacenas los datos en sessionStorage
-
-    // También puedes limpiar cualquier otro estado de autenticación en tu aplicación si es necesario
-
-    // Redirigir al usuario a la página de inicio de sesión
-    this.router.navigate(['/login']);  // Aquí puedes redirigir a la página de login
+    localStorage.removeItem('user'); 
+    sessionStorage.removeItem('user'); 
+    this.router.navigate(['/login']); 
   }
 }
